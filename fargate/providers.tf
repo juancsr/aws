000dd6a16e94c595f3061ee88d8603fe427ebc56
juancsr <juan.sarmiento@gorillalogic.com>
2025-08-12 19:37:00 -0500

    Assuming role with provider

--- fargate/providers.tf
+++ fargate/providers.tf
@@ -10,4 +10,16 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+
+  default_tags {
+    tags = {
+      "Environment" = "Development"
+      "Project"     = "FargateExample"
+    }
+  }
+
+  assume_role {
+    role_arn = "arn:aws:iam::138021360462:role/terraform-fargate-example"
+    session_name = "fargate-example-session"
+  }
 }
